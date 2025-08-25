ce91705f43467bf99240c0b7bffb89911252730f
Petro <user@MacBook-Pro.local>
2025-08-25 13:13:53 +0200

    loop through files, do not use threads. Loop is fast enough anyway, but threads could stuck

--- Wappalyzer/data/update.py
+++ Wappalyzer/data/update.py
@@ -1,42 +1,57 @@
 import requests
 import json
 from typing import Dict, List
-from concurrent.futures import ThreadPoolExecutor
 
 WappalyzerRoot = "https://raw.githubusercontent.com/enthec/webappanalyzer/main/src"
+
 
 def fetch_technologies(files: List[str]) -> Dict[str, Dict]:
     """
     Fetches technology data files in parallel and returns a compiled dictionary of technologies.
-    
+
     Args:
         files (List[str]): List of file names to fetch.
-    
+
     Returns:
         Dict[str, Dict]: Compiled dictionary of technologies.
     """
     technologies = {}
 
-    def fetch_file(f: str) -> None:
-        """
-        Fetches a single technology file and updates the shared dictionary.
+    # from concurrent.futures import ThreadPoolExecutor
+    # def fetch_file(f: str) -> None:
+    #     """
+    #     Fetches a single technology file and updates the shared dictionary.
 
-        Args:
-            f (str): The file name to fetch.
-        """
+    #     Args:
+    #         f (str): The file name to fetch.
+    #     """
+    #     url = f"{WappalyzerRoot}/technologies/{f}.json"
+    #     try:
+    #         resp = requests.get(url)
+    #         resp.raise_for_status()
+    #         m = resp.json()
+    #         technologies.update(m)
+    #     except Exception as e:
+    #         print(f"Failed to download or parse {f}.json: {e}")
+
+    # with ThreadPoolExecutor() as executor:
+    #     executor.map(fetch_file, files)
+    # NOTE loop through files, do not use threads.
+    # Loop is fast enough anyway, but threads could stuck
+    for f in files:
         url = f"{WappalyzerRoot}/technologies/{f}.json"
+        print(f"Fetching {url}")
         try:
             resp = requests.get(url)
             resp.raise_for_status()
             m = resp.json()
             technologies.update(m)
+            print(f"Successfully fetched {url}")
         except Exception as e:
             print(f"Failed to download or parse {f}.json: {e}")
 
-    with ThreadPoolExecutor() as executor:
-        executor.map(fetch_file, files)
-    
     return technologies
+
 
 def fetch_categories() -> Dict[str, Dict]:
     """
@@ -55,6 +70,7 @@ def fetch_categories() -> Dict[str, Dict]:
         return {}
     return categories
 
+
 def get_technology_data() -> Dict[str, Dict]:
     """
     Gets and Compiles categories and technologies into a single dictionary structure.
@@ -63,15 +79,46 @@ def get_technology_data() -> Dict[str, Dict]:
         Dict[str, Dict]: Compiled technology data.
     """
     categories = fetch_categories()
-    files = ["_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    files = [
+        "_",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
     technologies = fetch_technologies(files)
 
     technology_data = {"technologies": technologies, "categories": categories}
 
     return technology_data
 
+
 if __name__ == "__main__":
 
     # Example usage:
     technology_data = get_technology_data()
-    print(json.dumps(technology_data, indent=2))  # For demonstration; replace with desired use.
+    print(
+        json.dumps(technology_data, indent=2)
+    )  # For demonstration; replace with desired use.
